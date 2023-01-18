5d61ae2b346c24e1978f3db60fa93db65643e040
NicolaMurtasAlp <nmurtas@alpenite.com>
2023-01-18 09:43:30 +0100

    esempi SlingPostProcess

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/services/imp/SlingPostProcessorExample.java
@@ -0,0 +1,60 @@
+package com.adobe.aem.guides.wknd.core.services.imp;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.apache.sling.servlets.post.Modification;
+import org.apache.sling.servlets.post.SlingPostProcessor;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class SlingPostProcessorExample implements SlingPostProcessor {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SlingPostProcessorExample.class);
+
+    @Reference
+    private ResourceResolverFactory resourceResolverFactory;
+
+    @Override
+    public void process(SlingHttpServletRequest request, List<Modification> mods) throws Exception {
+
+
+        try (ResourceResolver resourceResolver = request.getResourceResolver()) {
+
+            /**
+             * This map will be used to get session via getServiceResourceResolver() method
+             */
+            Map<String, Object> params = new HashMap<>();
+
+            /**
+             * Adding the subservice name in the param map
+             */
+            params.put(ResourceResolverFactory.SUBSERVICE, "wknd-service-admin");
+
+            /**
+             * Getting resource resolver from the service factory
+             */
+            ResourceResolver resolver = resourceResolverFactory.getServiceResourceResolver(params);
+
+
+            Resource resourceBeforeModification1 = resolver.getResource(request.getResource().getPath());
+            //current state of the resource before changes are persisted
+            Resource resourceBeforeModification = resourceResolver.getResource(request.getResource().getPath());
+            //our resource will be in the below state after changes are persisted
+            Resource resourceAfterModification = request.getResource();
+
+        } catch (Exception e) {
+            LOGGER.error("login error", e);
+        }
+
+    }
+
+}
